582dd4ba79ec73b18a7de9caa1d4172c4acef196
KSmp <p2511698@my365.dmu.ac.uk>
2022-05-20 11:59:20 +0100

    Finish

--- app.js
+++ app.js
@@ -127,6 +127,10 @@ function post(req, res, target, page) {
 function getOne(req, res, target, fields = {}) {
   db.collection(target).findOne( { "name": req.params.name }, { projection: { _id:0, ...fields }})
     .then(result => {
+      if (!result) {
+        res.send(null)
+        return
+      }
       res.send(result);
     })
     .catch(error => catchError(error))
@@ -175,8 +179,16 @@ function createPost(req) {
 app.get('/user-groups/:name', (req, res) => {
   db.collection(collection.users).findOne({ "name": req.params.name }, { projection: { _id:0, groups:1 } })
     .then(uG => {
-      db.collection(collection.groups).find({"name" : {"$in" : uG.groups}}).project({ _id:0, name:1, img:1 }).toArray()
-        .then(result => { 
+      if (!uG) {
+        res.send(null)
+        return
+      }
+      db.collection(collection.groups).find({"name" : {"$in" : uG.groups}}).project({ _id:0, name:1, img:1, slug:1 }).toArray()
+        .then(result => {
+          if (!result) {
+            res.send(null)
+            return
+          }
           res.send(result);
         })
         .catch(error => catchError(error))
@@ -187,8 +199,16 @@ app.get('/user-groups/:name', (req, res) => {
 app.get('/available-groups/:name', (req, res) => {
   db.collection(collection.users).findOne({ "name": req.params.name }, { projection: { _id:0, groups:1 } })
     .then(uG => {
+      if (!uG) {
+        res.send(null)
+        return
+      }
       db.collection(collection.groups).find({"name" : {"$nin" : uG.groups}}).project({ _id:0, name:1, img:1, background:1 }).toArray()
-        .then(result => { 
+        .then(result => {
+          if (!result) {
+            res.send(null)
+            return
+          }
           res.send(result);
         })
         .catch(error => catchError(error))
@@ -196,12 +216,25 @@ app.get('/available-groups/:name', (req, res) => {
     .catch(error => catchError(error))
 })
 
-app.get('/group/:name', (req, res) => {
+app.get('/groups/:name', (req, res) => {
   getOne(req, res, collection.groups, { comments:0 })
 })
 
-app.get('/post/:parentType/:parent/:name', (req, res) => {
-  getOne(req, res, getTarget(req.params.parentType), { post:1 })
+app.get('/post/:parentType/:parent/:title', (req, res) => {
+  db.collection(getTarget(req.params.parentType)).findOne( { "name": req.params.parent }, { projection: { _id:0, posts:1 }})
+    .then(result => {
+      if (!result) {
+        res.send(null)
+        return
+      }
+      result.posts.forEach(post => {
+        if (post.slug == req.params.title)  {
+          res.send(post)
+          return
+        }
+      })
+    })
+    .catch(error => catchError(error))
 })
 
 app.get('/friends/:name', (req, res) => {
@@ -221,26 +254,41 @@ app.get('/user/:name', (req, res) => {
 })
 
 app.post('/login', (req, res) => {
-  db.collection(collection.accounts).findOne({ "name": req.body.username }, { projection: { _id:0, password:1 } })
+  db.collection(collection.accounts).findOne({ 'name': req.body.name }, { projection: { _id:0, password:1 } })
     .then(result => {
-      if (req.body.password == result.password) {
-        res.send({ isLoggedIn: true, name: req.body.username })
-      } else {
-        res.send({ isLoggedIn: false, name: null })
+      if (result) {
+        if (req.body.password == result.password) {
+          res.send({ isLoggedIn: true, name: req.body.name })
+          return
+        } 
       }
+
+      res.send({ isLoggedIn: false, name: null })
     })
     .catch(error => catchError(error))
 })
 
 app.post('/register', (req, res) => {
-  db.collection(collection.accounts).countDocuments({ 'name': req.body.username })
+  db.collection(collection.accounts).countDocuments({ 'name': req.body.name })
     .then(result => {
       if (parseInt(result) > 0) {
         res.status(409).send({ created: false })
       } else {
         db.collection(collection.accounts).insertOne(req.body, { forceServerObjectId: true })
           .then(result => {
-            res.status(201).send({ created: true });
+            const user = {
+              name: req.body.name,
+              groups: [],
+              posts: [],
+              friends: [],
+              games: [],
+              description: "I'm new!",
+            }
+            db.collection(collection.users).insertOne(user, { forceServerObjectId: true })
+              .then(result => {
+                res.status(201).send({ created: true });
+              })
+              .catch(error => catchError(error))
           })
           .catch(error => catchError(error))
       }
@@ -249,14 +297,31 @@ app.post('/register', (req, res) => {
 })
 
 app.post('/create-group', (req, res) => {
-  db.collection(collection.groups).countDocuments({ 'name': req.body.groups })
+  db.collection(collection.groups).countDocuments({ 'name': req.body.name })
     .then(result => {
       if (parseInt(result) > 0) {
         res.status(409).send({ created: false })
       } else {
-        db.collection(collection.groups).insertOne(req.body, { forceServerObjectId: true })
+        const group = {
+          slug: slugify(req.body.name),
+          name: req.body.name,
+          img: req.body.img,
+          usersCount: 1,
+          access: 1,
+          games: [],
+          description: req.body.description,
+          background: req.body.background,
+          posts: [],
+          owner: req.body.owner,
+          users: [req.body.owner]
+        }
+        db.collection(collection.groups).insertOne(group, { forceServerObjectId: true })
           .then(result => {
-            res.status(201).send({ created: true });
+            db.collection(collection.users).findOneAndUpdate({ 'name': group.owner }, { $push: { 'groups': group.slug }})
+              .then(result => {
+                res.status(201).send({ created: true });
+              })
+              .catch(error => catchError(error))
           })
           .catch(error => catchError(error))
       }
@@ -266,25 +331,106 @@ app.post('/create-group', (req, res) => {
 
 app.post('/create-post/:parentType/:parent', (req, res) => {
   let post = createPost(req)
-  db.collection(getTarget(req.params.parentType)).countDocuments({ 'title': req.body.title, 'name': req.params.parent })
-    .then(result => {
-      if(parseInt(result) > 0) {
-        post.slug = post.slug + "-" + (parseInt(result) + 1);
+  db.collection(collection.users).findOne({ "name": req.body.author.name }, { projection: { _id:0, img:1 }})
+    .then(avatar => {
+      if (avatar?.img) {
+        post.author.img = avatar.img
       }
-      
-      db.collection(getTarget(req.params.parentType)).findOneAndUpdate({ slug: req.params.slug}, update)
+      db.collection(getTarget(req.params.parentType)).countDocuments({ 'title': req.body.title, 'name': req.params.parent })
         .then(result => {
-          res.status(200).send();
-        })
-        .catch(error => catchError(error))
+          if (parseInt(result) > 0) {
+            post.slug = post.slug + "-" + (parseInt(result) + 1);
+          }
 
-      db.collection(getTarget(req.params.parentType)).insertOne(post, {forceServerObjectId: true})
-        .then(result => {
-          res.status(201).send({ created: true });
+          db.collection(getTarget(req.params.parentType)).findOneAndUpdate({ 'name': req.params.parent }, { $push: { 'posts': post }})
+            .then(result => {
+              res.status(201).send({ created: true });
+            })
+            .catch(error => catchError(error))
         })
         .catch(error => catchError(error))
     })
     .catch(error => catchError(error))
+})
+
+app.post('/edit-group/:group', (req, res) => {
+  console.log(req.body)
+  let updatedGroup = {
+    description: req.body.description,
+    img: req.body.img,
+    background: req.body.background,
+    games: req.body.games.split(',')
+  }
+  db.collection(collection.groups).findOneAndUpdate({ 'slug': req.params.group } , { "$set": updatedGroup })
+    .then(result => {
+      res.status(200).send()
+    })
+    .catch(error => catchError(error))
+})
+
+app.post('/edit-user/:user', (req, res) => {
+  console.log(req.body)
+  let updatedUser = {
+    description: req.body.description,
+    img: req.body.img,
+    background: req.body.background,
+    games: req.body.games.split(',')
+  }
+  db.collection(collection.users).findOneAndUpdate({ 'name': req.params.user } , { "$set": updatedUser })
+    .then(result => {
+      res.status(200).send()
+    })
+    .catch(error => catchError(error))
+})
+
+app.post('/join-group', (req, res) => {
+  console.log(req.body)
+  if (req.body.add) {
+    db.collection(collection.groups).findOneAndUpdate({ 'slug': req.body.group } , { $push: { 'users': req.body.user }, $inc: { 'usersCount': 1 } })
+      .then(result => {
+        db.collection(collection.users).findOneAndUpdate({ 'name': req.body.user } , { $push: { 'groups': req.body.group }})
+          .then(result => {
+            res.send({ changed: true })
+          })
+          .catch(error => catchError(error))
+      })
+      .catch(error => catchError(error))
+  } else {
+    db.collection(collection.groups).findOneAndUpdate({ 'slug': req.body.group } , { $pull: { 'users': req.body.user }, $inc: { 'usersCount': -1 } })
+      .then(result => {
+        db.collection(collection.users).findOneAndUpdate({ 'name': req.body.user } , { $pull: { 'groups': req.body.group }})
+          .then(result => {
+            res.send({ changed: false })
+          })
+          .catch(error => catchError(error))
+      })
+      .catch(error => catchError(error))
+  }
+})
+
+app.post('/add-friend', (req, res) => {
+  console.log(req.body)
+  if (req.body.add) {
+    db.collection(collection.users).findOneAndUpdate({ 'name': req.body.username1 } , { $push: { 'friends': req.body.username2 }})
+      .then(result => {
+        db.collection(collection.users).findOneAndUpdate({ 'name': req.body.username2 } , { $push: { 'friends': req.body.username1 }})
+          .then(result => {
+            res.send({ changed: true })
+          })
+          .catch(error => catchError(error))
+      })
+      .catch(error => catchError(error))
+  } else {
+    db.collection(collection.users).findOneAndUpdate({ 'name': req.body.username1 } , { $pull: { 'friends': req.body.username2 }})
+      .then(result => {
+        db.collection(collection.users).findOneAndUpdate({ 'name': req.body.username2 } , { $pull: { 'friends': req.body.username1 }})
+          .then(result => {
+            res.send({ changed: false })
+          })
+          .catch(error => catchError(error))
+      })
+      .catch(error => catchError(error))
+  }
 })
 
 /**
